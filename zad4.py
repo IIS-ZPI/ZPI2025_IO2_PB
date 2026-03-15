b41271cab4ac3ce93c998952065de642662a338d
kw\kamil <251240@edu.p.lodz.pl>
2026-03-15 16:30:38 +0100

    Template for task 4

---
+++ zad4.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class IArithmeticsAdd(ABC):
+    @abstractmethod
+    def addition(self, A: float, B: float) -> float:
+        pass
+
+
+class IArithmeticsDiff(ABC):
+    @abstractmethod
+    def difference(self, A: float, B: float) -> float:
+        pass
+
+
+class IArithmeticsMult(ABC):
+    @abstractmethod
+    def multiplication(self, A: float, B: float) -> float:
+        pass
+
+
+class IArithmeticsDiv(ABC):
+    @abstractmethod
+    def division(self, A: float, B: float) -> float:
+        pass
+
+
+class IArithmeticsPow(ABC):
+    @abstractmethod
+    def power(self, A: float, B: float) -> float:
+        pass
